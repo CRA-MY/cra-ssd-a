e1e01c3977ae7af49380a824cf0d607bacdba5d4
dw.koh <dw0402.koh@samsung.com>
2024-05-03 15:57:38 +0900

    feat: [Test code] read_write_position 함수검증

--- src/test/java/hardware/SSDFileReaderTest.java
+++ src/test/java/hardware/SSDFileReaderTest.java
@@ -25,6 +25,17 @@ class SSDFileReaderTest {
     @Test
     public void read_non_write_position() {
         SSDFileReader reader = new SSDFileReader();
+        int position = 0;
+
+        String actual = reader.read(position);
+        String expected = "0x00000000";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void read_write_position() {
+        SSDFileReader reader = new SSDFileReader();
         int position = 3;
 
         String actual = reader.read(position);

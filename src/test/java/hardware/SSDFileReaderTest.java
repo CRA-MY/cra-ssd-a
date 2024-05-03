50b1d3043fffcfbbfb749d8909882773c64b83b8
dw.koh <dw0402.koh@samsung.com>
2024-05-03 15:46:06 +0900

    refact: [Test code] read_non_write_position 검증

--- src/test/java/hardware/SSDFileReaderTest.java
+++ src/test/java/hardware/SSDFileReaderTest.java
@@ -2,13 +2,30 @@ package hardware;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SSDFileReaderTest {
+
     @Test
-    public void nand_txt_파일이_없는_경우() {
+    public void nand_txt_file_not_exists() {
         SSDFileReader reader = new SSDFileReader();
         int position = 0;
+
+        String actual = reader.read(position);
+        String expected = "0x00000000";
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void read_non_write_position() {
+        SSDFileReader reader = new SSDFileReader();
+        int position = 3;
 
         String actual = reader.read(position);
         String expected = "0x00000000";

7d5605d1e3e9d2890d5942739e3c187dd043b0b8
dw.koh <dw0402.koh@samsung.com>
2024-05-03 15:17:50 +0900

    feat: [Main code] nand_txt_파일이_없는_경우 검증

--- src/main/java/hardware/SSDFileReader.java
+++ src/main/java/hardware/SSDFileReader.java
@@ -1,7 +1,13 @@
 package hardware;
 
+import java.io.File;
+
 public class SSDFileReader {
     public String read(int position) {
+        File file = new File("nand.txt");
+        if(!file.exists()) {
+            return "0x00000000";
+        }
         return null;
     }
 }

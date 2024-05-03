5be6d088c24e5af6e1bb33d7ce70e9444b51c2d7
dw.koh <dw0402.koh@samsung.com>
2024-05-03 15:59:18 +0900

    refact: [Test code] read_write_position 함수검증

--- src/test/java/hardware/SSDFileReaderTest.java
+++ src/test/java/hardware/SSDFileReaderTest.java
@@ -1,22 +1,22 @@
 package hardware;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SSDFileReaderTest {
 
+    SSDFileReader reader;
+
+    @BeforeEach
+    public void setUp() {
+        reader = new SSDFileReader();
+    }
+
     @Test
     public void nand_txt_file_not_exists() {
-        SSDFileReader reader = new SSDFileReader();
-        int position = 0;
-
-        String actual = reader.read(position);
+        String actual = reader.read(0);
         String expected = "0x00000000";
 
         assertEquals(expected, actual);
@@ -24,10 +24,7 @@ class SSDFileReaderTest {
 
     @Test
     public void read_non_write_position() {
-        SSDFileReader reader = new SSDFileReader();
-        int position = 0;
-
-        String actual = reader.read(position);
+        String actual = reader.read(0);
         String expected = "0x00000000";
 
         assertEquals(expected, actual);
@@ -35,10 +32,7 @@ class SSDFileReaderTest {
 
     @Test
     public void read_write_position() {
-        SSDFileReader reader = new SSDFileReader();
-        int position = 3;
-
-        String actual = reader.read(position);
+        String actual = reader.read(3);
         String expected = "0x12345678";
 
         assertEquals(expected, actual);

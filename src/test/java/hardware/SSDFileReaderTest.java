3c11646c5955dbc95bd61ff5e5ad64721c3d66d2
dw.koh <dw0402.koh@samsung.com>
2024-05-03 16:28:46 +0900

    feat: [Test code] Test용 임시 nand.txt 파일 생성/삭제 기능 추가

--- src/test/java/hardware/SSDFileReaderTest.java
+++ src/test/java/hardware/SSDFileReaderTest.java
@@ -3,6 +3,11 @@ package hardware;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SSDFileReaderTest {
@@ -14,8 +19,30 @@ class SSDFileReaderTest {
         reader = new SSDFileReader();
     }
 
+    public void makeTestNandFile() {
+        try {
+            FileWriter fileWriter = new FileWriter("nand.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for(int i=0; i<100; i++) {
+                if (i == 3)
+                    bufferedWriter.write("0x12345678\n");
+                else
+                    bufferedWriter.write("0x00000000\n");
+            }
+            bufferedWriter.close();
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void deleteTestNandFile() {
+        File file = new File("nand.txt");
+        file.delete();
+    }
+
     @Test
     public void nand_txt_file_not_exists() {
+        deleteTestNandFile();
         String actual = reader.read(0);
         String expected = "0x00000000";
 
@@ -24,6 +51,7 @@ class SSDFileReaderTest {
 
     @Test
     public void read_non_write_position() {
+        makeTestNandFile();
         String actual = reader.read(0);
         String expected = "0x00000000";
 
@@ -32,6 +60,7 @@ class SSDFileReaderTest {
 
     @Test
     public void read_write_position() {
+        makeTestNandFile();
         String actual = reader.read(3);
         String expected = "0x12345678";
 

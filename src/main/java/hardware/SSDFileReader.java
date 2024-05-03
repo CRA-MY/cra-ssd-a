4f0f4ddcecc198588b3bb2446d551a6461c42519
dw.koh <dw0402.koh@samsung.com>
2024-05-03 15:35:49 +0900

    refact: [Main code] nand_txt_파일이_없는_경우 검증2

--- src/main/java/hardware/SSDFileReader.java
+++ src/main/java/hardware/SSDFileReader.java
@@ -1,6 +1,9 @@
 package hardware;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class SSDFileReader {
     private static final String DEFAULT_VALUE = "0x00000000";
@@ -8,11 +11,24 @@ public class SSDFileReader {
 
     public String read(int position) {
         if(storeFileNotExists())
-            return DEFAULT_VALUE;
+            return writeResultFile(DEFAULT_VALUE);
         return null;
     }
 
     private boolean storeFileNotExists() {
         return !new File(STORE_FILE_NAME).exists();
+    }
+
+    private String writeResultFile(String value) {
+        FileWriter fileWriter = null;
+        try {
+            fileWriter = new FileWriter("result.txt");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write(value);
+            bufferedWriter.close();
+            return value;
+        } catch (IOException e) {
+            return DEFAULT_VALUE;
+        }
     }
 }

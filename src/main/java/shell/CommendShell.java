425b13229a89d52d282400b4683cba1f6c4bba4a
ycyoondev <z20152002@gmail.com>
2024-05-03 15:31:49 +0900

    feat: [Main code] 사용자 입력 코드

--- src/main/java/shell/CommendShell.java
+++ src/main/java/shell/CommendShell.java
@@ -1,7 +1,18 @@
 package shell;
 
+import java.util.Scanner;
+
 public class CommendShell {
     public static void main(String[] args) {
+        String input;
+        do {
+            input = getInput();
+        } while (!input.equals("exit"));
+    }
 
+    public static String getInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter input: ");
+        return scanner.nextLine();
     }
 }

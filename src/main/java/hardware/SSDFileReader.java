64c9246120bde30e5ab76f3530e1e3f006c033c3
dongwan-koh <livera89@gmail.com>
2024-05-07 10:08:54 +0900

    feat: [Main code] write_result_file 함수구현 변경

--- src/main/java/hardware/SSDFileReader.java
+++ src/main/java/hardware/SSDFileReader.java
@@ -1,37 +1,50 @@
 package hardware;
 
 import java.io.*;
+import java.util.ArrayList;
 
 public class SSDFileReader {
     private static final String DEFAULT_VALUE = "0x00000000";
     private static final String STORE_FILE_NAME = "nand.txt";
-    private static final String RESULT_FILE_NAME = "result.txt";
 
-    private SSDFileWriter ssdFileWriter;
-
-    public SSDFileReader() {
-        ssdFileWriter = new SSDFileWriter();
-    }
-
-    public String read(int position){
+    public static String read(int position){
         try{
             String value = readNandFile(position);
-            ssdFileWriter.writeResultFile(value);
+            SSDFileWriter.writeResultFile(value);
             return value;
         } catch (IOException io) {
             return DEFAULT_VALUE;
         }
     }
 
-    private String readNandFile(int position) throws IOException {
-        String result = "";
-        FileReader fileReader = new FileReader(STORE_FILE_NAME);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-        for(int i = 0; i<= position; i++)
-            result = bufferedReader.readLine();
-        bufferedReader.close();
-        return result;
+    private static String readNandFile(int position){
+        try {
+            String result = "";
+            FileReader fileReader = new FileReader(STORE_FILE_NAME);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            for (int i = 0; i <= position; i++)
+                result = bufferedReader.readLine();
+            bufferedReader.close();
+            return result;
+        } catch (IOException e) {
+            return DEFAULT_VALUE;
+        }
     }
 
 
+    public static ArrayList<String> readNandAllContents(){
+        try {
+            ArrayList<String> result = new ArrayList<>();
+            FileReader fileReader = new FileReader(STORE_FILE_NAME);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            for(int i = 0; i< 100; i++)
+                result.add(bufferedReader.readLine());
+            return result;
+        } catch (IOException e) {
+            ArrayList<String> result = new ArrayList<>();
+            for(int i = 0; i< 100; i++)
+                result.add(DEFAULT_VALUE);
+            return result;
+        }
+    }
 }
--- src/main/java/hardware/SSDFileWriter.java
+++ src/main/java/hardware/SSDFileWriter.java
@@ -3,21 +3,34 @@ package hardware;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class SSDFileWriter {
     /*
-    1. nand.txt 파일에 내용일 읽어서 ArrayList<String>에 저장한다.
+    1. nand.txt 파일에 내용을 읽어서 ArrayList<String>에 저장한다.
     2. input으로 들어온 position에 값을 value로 바꿔준다.
     3. nana.txt에 변경된 ArrayList<String>로 새로 쓴다.
     */
     private static final String DEFAULT_VALUE = "0x00000000";
     private static final String RESULT_FILE_NAME = "result.txt";
+    private static final String STORE_FILE_NAME = "nand.txt";
 
-    public boolean write(int position, String value) {
-        return false;
+    public static boolean writeNandFile(int position, String value) {
+        try {
+            ArrayList<String> base_list = SSDFileReader.readNandAllContents();
+            base_list.set(position, value);
+            FileWriter fileWriter = new FileWriter(STORE_FILE_NAME);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            for(int i=0; i<base_list.size(); i++)
+                bufferedWriter.write(base_list.get(i) + "\n");
+            bufferedWriter.close();
+        } catch (IOException e) {
+            return false;
+        }
+        return true;
     }
 
-    public void writeResultFile(String value) throws IOException {
+    public static void writeResultFile(String value) throws IOException {
         FileWriter fileWriter = new FileWriter(RESULT_FILE_NAME);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         bufferedWriter.write(value);

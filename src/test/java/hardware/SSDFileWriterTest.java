f880b1b764a0e42ee638cb5afc21c5912889bf9f
dongwan-koh <livera89@gmail.com>
2024-05-07 10:49:25 +0900

    refact: [Main code, Test code] 호출부에 맞춰서 함수 이름 변경

--- src/test/java/hardware/SSDFileWriterTest.java
+++ src/test/java/hardware/SSDFileWriterTest.java
@@ -8,21 +8,17 @@ class SSDFileWriterTest {
 
     @Test
     public void write_result_file() {
-        SSDFileWriter ssdFileWriter = new SSDFileWriter();
-
         assertDoesNotThrow(() -> {
-            ssdFileWriter.writeResultFile("0x00000000");
+            SSDFileWriter.writeResultFile("0x00000000");
         });
     }
 
     @Test
     public void write_nand_file() {
-        SSDFileWriter ssdFileWriter = new SSDFileWriter();
-
         int position = 0;
         String value = "0x12345678";
 
-        SSDFileWriter.writeNandFile(position, value);
+        SSDFileWriter.write(position, value);
     }
 
 }

3705c096e34375b3b126846742a2c854bf527490
sejong <sejong.yu@samsung.com>
2024-05-07 14:59:21 +0900

    Add Validate Test Case4 ;

--- src/test/java/shell/ValidateTest.java
+++ src/test/java/shell/ValidateTest.java
@@ -66,8 +66,8 @@ class ValidateTest {
 
     @Test
     void testValidateWriteCommandWithValidInput() {
-        UserInput result = validate.validateCommand("WRITE 50 0x12345678");
-        assertEquals(50, result.getLBA());
+        UserInput result = validate.validateCommand("WRITE 50 0x123456GG");
+        assertEquals("PASS", result.getStatus());
     }
 
     @Test
@@ -107,6 +107,13 @@ class ValidateTest {
     @Test
     void testValidateValueWithInvalidLength() {
         assertFalse(validate.validateValue("0x123456"));
+    }
+
+    @Test
+    void testValidateValueWithInvalidWord() {
+
+        assertFalse(validate.validateValue("0x123456ee"));
+
     }
 
     @Test

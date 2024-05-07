24838f7d0856a29daafb904699f433c910d41e42
jmin.han <jmin.han@samsung.com>
2024-05-07 11:52:19 +0900

    refact: [Main code] testapp1/2 함수 - Method 추출 - 상수 추출 - TEST 실패 시 출력 정보 추가

--- src/main/java/shell/Service.java
+++ src/main/java/shell/Service.java
@@ -3,6 +3,8 @@ package shell;
 import hardware.IStorage;
 
 public class Service {
+    public static final String TESTAPP_2_INITIAL_WRITE_VALUE = "0xAAAABBBB";
+    public static final String TESTAPP_2_OVER_WRITE_VALUE = "0x12345678";
     IStorage iStorage;
 
     public Service(IStorage iStorage) {
@@ -35,37 +37,53 @@ public class Service {
 
     public void testapp1(String value) {
         fullwrite(value);
+        if (isWritten(value)) {
+            System.out.print("TestApp1 성공하였습니다.\n");
+        }
+    }
+
+    private boolean isWritten(String value) {
         for (int i = 0; i < 100; i++) {
             if (!iStorage.Read(i).equals(value)) {
                 System.out.print("TestApp1 실패.\n");
-                return;
+                System.out.print(i + "번 LBA에 " + value + "가 정상 Write 되지 않았습니다.\n");
+                return false;
             }
         }
-        System.out.print("TestApp1 성공하였습니다.\n");
+        return true;
     }
 
     public void testapp2() {
-        String testapp2WriteValue = "0xAAAABBBB";
-        String testapp2OverWriteValue = "0x12345678";
+        initialWrite30times(TESTAPP_2_INITIAL_WRITE_VALUE);
+        overWrite(TESTAPP_2_OVER_WRITE_VALUE);
+        if (isOverWritten(TESTAPP_2_OVER_WRITE_VALUE)) {
+            System.out.print("TestApp2 성공하였습니다.\n");
+        }
+    }
 
+    private void initialWrite30times(String value) {
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 30; j++) {
-                write(i, testapp2WriteValue);
+                write(i, value);
             }
         }
+    }
 
+    private boolean isOverWritten(String value) {
         for (int i = 0; i < 5; i++) {
-            write(i, testapp2OverWriteValue);
-        }
-
-        for (int i = 0; i < 5; i++) {
-            if (!iStorage.Read(i).equals(testapp2OverWriteValue)) {
+            if (!iStorage.Read(i).equals(value)) {
                 System.out.print("TestApp2 실패.\n");
-                return;
+                System.out.print(i + "번 LBA에 " + value + "가 정상 Over Write 되지 않았습니다.\n");
+                return false;
             }
         }
+        return true;
+    }
 
-        System.out.print("TestApp2 성공하였습니다.\n");
+    private void overWrite(String value) {
+        for (int i = 0; i < 5; i++) {
+            write(i, value);
+        }
     }
 
 }
